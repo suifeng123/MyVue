34d19a5402b23d41e76bbef80a1b84f0215fa3b3
wangshengwen <wswen1220@163.com>
2017-06-04 16:53:36 +0800

    MyVue

--- src/Vue.js
+++ src/Vue.js
@@ -936,5 +936,56 @@ Observer.prototype.observeArray = function observeArray(items) {
      return ob
   }
 
+  function defineReactive$$1 (
+      obj,
+      key,
+      val,
+      customSetter
+  ){
+     var dep = new Dep();
+     var property = Object.getOwnPropertyDescriptor(obj,key);
+     if(property && property.configurable === false){
+         return
+     }
+     var getter = property && property.get;
+     var setter = property && property.set;
+     var childOb = observe(val);
+     Object.defineProperty(obj,key,{
+         enumberable: true,
+        configurable: true,
+         get: function reactiveGetter() {
+             var value = getter?getter.call(obj):val;
+             if(Dep.target){
+                dep.depend();
+                if(childOb){
+                    childOb.dep.depend();
+                }
+                if(Array.isArray(value)){
+                    dependArray(value);
+                }
+             }
+             return value
+         },
+         set: function reactiveSetter(newVal) {
+             var value = getter?getter.call(obj):val;
+             if(newVal === value || (newVal!==newVal && value!==value)){
+                 return
+             }
+             if("development" !== 'production' && customSetter){
+                 customSetter();
+             }
+             if(setter){
+                 setter.call(obj,newVal);
+             }else{
+                 val = newVal;
+             }
+             childOb = observe(newVal);
+             dep.notify();
+         }
+     });
+  }
+  //写到源码882行
+
+
 
 })
