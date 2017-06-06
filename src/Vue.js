16b92b5d4f34b2de9bd24601293352914f0192f6
wangshengwen <wswen1220@163.com>
2017-06-06 14:24:51 +0800

    MyVue

--- src/Vue.js
+++ src/Vue.js
@@ -1409,6 +1409,135 @@ function normalizeProps(options) {
         }
     }
 
+    function assertType(value,type){
+        var valid;
+        var expectedType = getType(type);
+        if(expectedType === 'String'){
+            valid = typeof value ===(expectedType='string');
+        }else if(expectedType === 'Number'){
+            valid = typeof value === (expectedType='number');
+        }else if(expectedType === 'Function'){
+            valid = typeof value ===(expectedType='function');
+        }else if(expectedType==='Object'){
+            valid = isPlainObject(value);
+        }else if(expectedType === 'Array'){
+           valid = Array.isArray(value);
+        }else {
+            valid = value instanceof type;
+        }
+        return {
+            valid: valid,
+            expectedType: expectedType
+        }
+    }
+
+
+    function getType (fn) {
+        var match = fn && fn.toString().match(/^\s*function (\w+)/);
+        return match && match[1]
+    }
+
+    function isType (type, fn) {
+        if (!Array.isArray(fn)) {
+            return getType(fn) === getType(type)
+        }
+        for (var i = 0, len = fn.length; i < len; i++) {
+            if (getType(fn[i]) === getType(type)) {
+                return true
+            }
+        }
+        /* istanbul ignore next */
+        return false
+    }
+
+
+    function handleError (err,vm,info){
+        if(config.errorHandler){
+            config.errorHandler.call(null,err,vm,info);
+        }else {
+            {
+                warn(("Error in"+info+":"),vm);
+            }
+          if(inBrowser  && typeof console !== 'undefined'){
+                console.error(err);
+          }else{
+              throw  err;
+          }
+        }
+    }
+    var initProxy;
+
+    {
+        var allowedGlobals = makeMap(
+            'Infinity,undefined,NaN,isFinite,isNaN,' +
+            'parseFloat,parseInt,decodeURI,decodeURIComponent,encodeURI,encodeURIComponent,' +
+            'Math,Number,Date,Array,Object,Boolean,String,RegExp,Map,Set,JSON,Intl,' +
+            'require' // for Webpack/Browserify
+        );
+
+        var warnNonPresent = function (target, key) {
+            warn(
+                "Property or method \"" + key + "\" is not defined on the instance but " +
+                "referenced during render. Make sure to declare reactive data " +
+                "properties in the data option.",
+                target
+            );
+        };
+
+        var hasProxy =
+            typeof Proxy !== 'undefined' &&
+            Proxy.toString().match(/native code/);
+
+        if (hasProxy) {
+            var isBuiltInModifier = makeMap('stop,prevent,self,ctrl,shift,alt,meta');
+            config.keyCodes = new Proxy(config.keyCodes, {
+                set: function set (target, key, value) {
+                    if (isBuiltInModifier(key)) {
+                        warn(("Avoid overwriting built-in modifier in config.keyCodes: ." + key));
+                        return false
+                    } else {
+                        target[key] = value;
+                        return true
+                    }
+                }
+            });
+        }
+
+        var hasHandler = {
+            has: function has (target, key) {
+                var has = key in target;
+                var isAllowed = allowedGlobals(key) || key.charAt(0) === '_';
+                if (!has && !isAllowed) {
+                    warnNonPresent(target, key);
+                }
+                return has || !isAllowed
+            }
+        };
+
+        var getHandler = {
+            get: function get (target, key) {
+                if (typeof key === 'string' && !(key in target)) {
+                    warnNonPresent(target, key);
+                }
+                return target[key]
+            }
+        };
+
+        initProxy = function initProxy (vm) {
+            if (hasProxy) {
+                // determine which proxy handler to use
+                var options = vm.$options;
+                var handlers = options.render && options.render._withStripped
+                    ? getHandler
+                    : hasHandler;
+                vm._renderProxy = new Proxy(vm, handlers);
+            } else {
+                vm._renderProxy = vm;
+            }
+        };
+    }
+
+
 
 
 
