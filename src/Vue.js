44e9da17275cfcc537b053c7bd6caed5a990f5d6
wangshengwen <wswen1220@163.com>
2017-06-12 16:14:31 +0800

    MyVue

--- src/Vue.js
+++ src/Vue.js
@@ -3063,6 +3063,183 @@ function checkProp(
     return false;
 }
 
+function mergeHooks(data) {
+    if(!data.hook){
+        data.hook = {};
+    }
+    for(var i=0;i < hooksToMerge.length;i++){
+        var key = hooksToMerge[i];
+        var fromParent = data.hook[key];
+        var ours = componentVNodeHooks[key];
+        data.hook[key] = fromParent ? mergeHook$1(ours,fromParent):ours;   
+    }
+}
+
+function mergeHook$1(one,two){
+    return function(a,b,c,d){
+        one(a,b,c,d);
+        two(a,b,c,d);
+    }
+}
+
+var SIMPLE_NORMALIZE = 1;
+var ALWAYS_NORMALIZE = 2;
+
+function createElement(
+    context,
+    tag,
+    data,
+    children,
+    normalizationType,
+    alwaysNormalize){
+    if(Array.isArray(data) || isPrimitive(data)){
+        normalizationType = children;
+        children = data;
+        data = undefined;
+    }
+    if(alwaysNormalize) {normalizationType = ALWAYS_NORMALIZE;}
+    return _createElement(context,tag,data,children,normalizationType)
+}
+
+function _createElement (
+  context,
+  tag,
+  data,
+  children,
+  normalizationType
+) {
+  if (data && data.__ob__) {
+    "development" !== 'production' && warn(
+      "Avoid using observed data object as vnode data: " + (JSON.stringify(data)) + "\n" +
+      'Always create fresh vnode data objects in each render!',
+      context
+    );
+    return createEmptyVNode()
+  }
+  if (!tag) {
+    // in case of component :is set to falsy value
+    return createEmptyVNode()
+  }
+  // support single function children as default scoped slot
+  if (Array.isArray(children) &&
+      typeof children[0] === 'function') {
+    data = data || {};
+    data.scopedSlots = { default: children[0] };
+    children.length = 0;
+  }
+  if (normalizationType === ALWAYS_NORMALIZE) {
+    children = normalizeChildren(children);
+  } else if (normalizationType === SIMPLE_NORMALIZE) {
+    children = simpleNormalizeChildren(children);
+  }
+  var vnode, ns;
+  if (typeof tag === 'string') {
+    var Ctor;
+    ns = config.getTagNamespace(tag);
+    if (config.isReservedTag(tag)) {
+      // platform built-in elements
+      vnode = new VNode(
+        config.parsePlatformTagName(tag), data, children,
+        undefined, undefined, context
+      );
+    } else if ((Ctor = resolveAsset(context.$options, 'components', tag))) {
+      // component
+      vnode = createComponent(Ctor, data, context, children, tag);
+    } else {
+      // unknown or unlisted namespaced elements
+      // check at runtime because it may get assigned a namespace when its
+      // parent normalizes children
+      vnode = new VNode(
+        tag, data, children,
+        undefined, undefined, context
+      );
+    }
+  } else {
+    // direct component options / constructor
+    vnode = createComponent(tag, data, context, children);
+  }
+  if (vnode) {
+    if (ns) { applyNS(vnode, ns); }
+    return vnode
+  } else {
+    return createEmptyVNode()
+  }
+}
+
+function applyNS(vnode,ns){
+    vnode.ns = ns;
+    if(vnode.tag === 'foreignObject') {
+
+        return
+    }
+    if(vnode.children){
+        for(var i=0,l=vnode.children.length;i<l;i++){
+            var child = vnode.children[i];
+            if(child.tag && !child.ns){
+                applyNS(child,ns);
+            }
+        }
+    }
+}
+
+function renderList (
+    val,
+    render) {
+    var ret,i,l,keys,key;
+    if(Array.isArray(val) || typeof val === 'string'){
+        ret = new Array(val.length);
+        for(i=0,l=val.length;i<l;i++){
+            ret[i] = render(val[i],i);
+        }
+    }else if(typeof val === 'number'){
+        ret = new Array(val.length);
+        for(i=0;i<val;i++){
+            ret[i] = render(i+1;i);
+        }
+    }else if(isObject(val)){
+        keys = Object.keys(val);
+        ret = new Array(keys.length);
+        for(i=0,l=keys.length;i<l;i++){
+            key = keys[i];
+            ret[i] = render(val[key],key,i);
+        }
+
+    }
+    return ret;
+}
+
+function renderSlot (
+  name,
+  fallback,
+  props,
+  bindObject
+) {
+  var scopedSlotFn = this.$scopedSlots[name];
+  if (scopedSlotFn) { // scoped slot
+    props = props || {};
+    if (bindObject) {
+      extend(props, bindObject);
+    }
+    return scopedSlotFn(props) || fallback
+  } else {
+    var slotNodes = this.$slots[name];
+    // warn duplicate slot usage
+    if (slotNodes && "development" !== 'production') {
+      slotNodes._rendered && warn(
+        "Duplicate presence of slot \"" + name + "\" found in the same render tree " +
+        "- this will likely cause render errors.",
+        this
+      );
+      slotNodes._rendered = true;
+    }
+    return slotNodes || fallback
+  }
+}
+
+
+
+
+
 
 
 
