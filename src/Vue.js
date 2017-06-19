95ac2a6243854ca80370ae4d7afda80d9a409d59
suifeng123 <wangshengwen31@gmail.com>
2017-06-19 16:42:42 +0800

    MyVue

--- src/Vue.js
+++ src/Vue.js
@@ -3528,7 +3528,33 @@ function hasParentTransition(vnode) {
     }
 
 
+var TransitionGroup = {
+    props: props,
+    reder: function render(h) {
+        var tag = this.tag || this.$vnode.data.tag || 'span';
+        var map = Object.create(null);
+        var prevChildren = this.prevChildren = this.children;
+        var rawChildren = this.$slot.default || [];
+        var children = this.children = [];
+        var transitionData = extractTransitionData(this);
 
+        for(var i=0;i<rawChildren.length;i++){
+            var c = rawChildren[i];
+            if(c.tag){
+                if(c.key != null && String(c.key).indexOf('__vlist')!==0){
+                    children.push(c);
+                    map[c.key] = c;
+                    (c.data||(c.data = {})).transition = transitionData;
+
+                }
+            }else {
+                var opts = c.componentOptions;
+                var name = opts ?(opts.Ctor.options.name || opts.tag || ''): c.tag;
+                warn(("<transition-group> children must be keyed: <" + name + ">"));
+            }
+        }
+    }
+}
 
 
 
