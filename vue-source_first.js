0a168200f96df59641b6dbdc9cde70981ccdac78
suifeng123 <wswen1220@163.com>
2017-10-10 16:41:33 +0800

    提交一个Vue的测试文件

--- vue-source_first.js
+++ vue-source_first.js
@@ -1,9 +1,15 @@
 /*
-研究一个()在javaScript中的作用
+研究一个()在javaScript中的作用,它的主要作用是封装作用域
  */
 
-(
-  var aa = "hello world";
-)
+
+/**
+ * 下面研究一下{}在程序中的作用
+ */
+
+{
+	var aa = "王胜文本";
+}
 
 console.log(aa);
+
